9c225d7d994e14c028974823eb29510b2eb7fc47
Ahmet Caner Tat <caner-tat@hotmail.com>
2023-11-12 18:37:54 +0100

    fixed file name

---
+++ CityInfo.Api/Migrations/20231103154535_Initial.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CityInfo.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PointsOfInterest",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    CityId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PointsOfInterest", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PointsOfInterest_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Where the FHV is located", "Dornbirn" },
+                    { 2, "The one with the cat tower", "Feldkirch" },
+                    { 3, "Last city before the Arlberg", "Bludenz" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "The best FH in the world", "FHV" },
+                    { 2, 1, "Second best beer in Vbg", "Mohren Brauerei" },
+                    { 3, 2, "Some old important building", "Cat tower" },
+                    { 4, 2, "Biiiiiiiiig schnitzel", "Schattenburg" },
+                    { 5, 3, "What else do you need?", "Schoki" },
+                    { 6, 3, "The worst beer in Vbg", "Fohrenburger Brauerei" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PointsOfInterest_CityId",
+                table: "PointsOfInterest",
+                column: "CityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PointsOfInterest");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
